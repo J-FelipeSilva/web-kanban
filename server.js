374a16c65be0dc9c812729b502ccd66ad0cca3b4
João <joao.felipesilva@outlook.com>
2026-08-12 14:44:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const sqlite3 = require('sqlite3');
+const express = require("express");
+const cors = require("cors");
+const sqlite3 = require("sqlite3");
